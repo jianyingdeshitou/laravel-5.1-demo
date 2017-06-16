508181321810d7a05224765c61b886d4bd8c543e
坚硬的石头 <do_nb@outlook.com>
2017-06-17 06:10:18 +0800

    [01-furbook.com] 添加 master 视图

---
+++ 01-furbook.com/app/furbook.com/resources/views/layouts/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Furbook</title>
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="page-header">
+            @yield('header')
+        </div>
+
+        @if (Session::has('success'))
+                    <div class="alert alert-success">
+                {{ Session::get('success') }}
+            </div>
+        @endif
+
+        @yield('content')
+    </div>
+</body>
+</html>
+
