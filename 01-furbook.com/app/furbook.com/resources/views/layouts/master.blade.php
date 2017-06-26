748d10bf0006e3c2791842e6cda49145ba8202e6
坚硬的石头 <do_nb@outlook.com>
2017-06-26 15:12:59 +0800

    [01-furbook.com] 修改 delete 操作问题

--- 01-furbook.com/app/furbook.com/resources/views/layouts/master.blade.php
+++ 01-furbook.com/app/furbook.com/resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
         </div>
 
         @if (Session::has('success'))
-                    <div class="alert alert-success">
+            <div class="alert alert-success">
                 {{ Session::get('success') }}
             </div>
         @endif
