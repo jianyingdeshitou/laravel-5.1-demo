15fd90b37d87b25c3040cce24dad4ac79f5d4520
坚硬的石头 <do_nb@outlook.com>
2017-06-26 14:16:46 +0800

    [01-furbook.com] 关闭 cats 数据库时间戳自动更新

--- 01-furbook.com/app/furbook.com/app/Cat.php
+++ 01-furbook.com/app/furbook.com/app/Cat.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Cat extends Model
 {
     //
+    public $timestamps = false;
+
     protected $fillable = ['name', 'date_of_birth', 'breed_id'];
 
     public function breed() {
