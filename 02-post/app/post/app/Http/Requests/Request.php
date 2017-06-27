d90c1f5134caf9f9494a73e8440399a8f0125bbe
坚硬的石头 <do_nb@outlook.com>
2017-06-27 14:26:02 +0800

    [02-post] 添加 post 项目

---
+++ 02-post/app/post/app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Post\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
---
+++ 02-post/srv/app/link.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm post
+ln -s ../../app/post post
+
+rm pma
+ln -s ../../app/pma pma
+
