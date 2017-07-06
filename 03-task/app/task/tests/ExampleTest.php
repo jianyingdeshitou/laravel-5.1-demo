6d43c15b32ce9832ed8a8c10843303dda8027c55
坚硬的石头 <do_nb@outlook.com>
2017-07-07 02:23:40 +0800

    [03-task] 修改基本的测试案例，使之能通过

--- 03-task/app/task/tests/ExampleTest.php
+++ 03-task/app/task/tests/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
     public function testBasicExample()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+            ->seePageIs('/auth/login')
+            ->assertResponseStatus(200);
     }
 }
