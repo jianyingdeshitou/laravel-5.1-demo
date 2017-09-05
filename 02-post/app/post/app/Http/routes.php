d22437a74612e6c9b9ce955c3d6b8b7b711ce988
坚硬的石头 <do_nb@outlook.com>
2017-09-06 03:01:11 +0800

    [02-post] 删除注册路由

--- 02-post/app/post/app/Http/routes.php
+++ 02-post/app/post/app/Http/routes.php
@@ -11,16 +11,14 @@
 |
 */
 
+// Blog
 Route::get('/', 'BlogController@index');
+Route::get('blog', 'BlogController@index');
 Route::get('blog/{slug}', 'BlogController@show');
 
 // 认证路由...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-// 注册路由...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 Route::get('profile','UserController@profile');
