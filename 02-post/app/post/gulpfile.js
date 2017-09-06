88edcd2a0835e0373377e3805eae0a74fdb27d07
坚硬的石头 <do_nb@outlook.com>
2017-09-07 04:45:39 +0800

    [02-post] 修改 gulp 规则

--- 02-post/app/post/gulpfile.js
+++ 02-post/app/post/gulpfile.js
@@ -1,4 +1,37 @@
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+
+gulp.task("copyfiles", function () {
+    var npm_path = 'node_modules/';
+    var res_path = 'resources/assets/';
+    var pub_path = 'public/'
+
+    // jquery
+    gulp.src(npm_path + 'jquery/dist/jquery.js')
+        .pipe(gulp.dest(res_path + "js/"));
+
+    // bootstrap
+    gulp.src(npm_path + 'bootstrap/dist/js/bootstrap.js')
+        .pipe(gulp.dest(res_path + "js/"));
+    gulp.src(npm_path + 'bootstrap/dist/fonts/**')
+        .pipe(gulp.dest(pub_path + "fonts/"));
+
+    // fontawesome
+    gulp.src(npm_path + 'font-awesome/fonts/**')
+        .pipe(gulp.dest(pub_path + "fonts/"));
+
+    // datatables
+    gulp.src(npm_path + 'datatables/media/js/jquery.dataTables.js')
+        .pipe(gulp.dest(res_path + "js/"));
+
+    // datatables-bootstrap3-plugin
+    gulp.src(npm_path + 'datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css')
+        .pipe(rename('datatables-bootstrap3.scss'))
+        .pipe(gulp.dest(res_path + "sass/"));
+    gulp.src(npm_path + 'datatables-bootstrap3-plugin/media/js/datatables-bootstrap3.js')
+        .pipe(gulp.dest(res_path + "js/"));
+});
 
 /*
  |--------------------------------------------------------------------------
