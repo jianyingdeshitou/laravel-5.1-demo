056cf9303005376a9bc5bd41ade8ebd701bd17c8
坚硬的石头 <do_nb@outlook.com>
2017-09-18 00:44:42 +0800

    [06-rbac] 完善 home 视图

--- 06-rbac/app/resources/views/home.blade.php
+++ 06-rbac/app/resources/views/home.blade.php
@@ -1,19 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 <div id="title" style="text-align: center;">
     <h1>Learn Laravel 5</h1>
     <div style="padding: 5px; font-size: 16px;">Learn Laravel 5</div>
