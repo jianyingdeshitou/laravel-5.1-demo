c5f02534fca69a56c7887dd606a5464da1b52f9f
坚硬的石头 <do_nb@outlook.com>
2017-09-17 18:04:40 +0800

    [06-rbac] 标准用户认证功能

--- 06-rbac/app/app/Http/routes.php
+++ 06-rbac/app/app/Http/routes.php
@@ -9,8 +9,21 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// 认证路由...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// 注册路由...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::get('home', function () {
+    return view('home');
 });
