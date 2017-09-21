b8e0cbbe4fae58c917d1d151f64baeed073a3353
坚硬的石头 <do_nb@outlook.com>
2017-09-22 04:17:18 +0800

    [06-rbac] 添加 /admin/create 视图处理

--- 06-rbac/app/app/Http/Controllers/Admin/ArticleController.php
+++ 06-rbac/app/app/Http/Controllers/Admin/ArticleController.php
@@ -20,7 +20,7 @@ class ArticleController extends Controller
     public function index()
     {
         //
-//        return view('admin.article.index')->withArticles(Article::all());
+        //        return view('admin.article.index')->withArticles(Article::all());
         return view('admin.article.index')
             ->withArticles(Article::where('user_id', Auth::user()->id)->get());
     }
@@ -33,6 +33,7 @@ class ArticleController extends Controller
     public function create()
     {
         //
+        return view('admin.article.create');
     }
 
     /**
@@ -43,7 +44,32 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // 数据验证
+        $this->validate($request, [
+            // 必填、在 articles 表中唯一、最大长度 255
+            'title' => 'required|unique:articles|max:255',
+            // 必填
+            'body' => 'required',
+        ]);
+
+        // 通过 Article Model 插入一条数据进 articles 表
+        // 初始化 Article 对象
+        $article = new Article;
+        // 将 POST 提交过了的 title 字段的值赋给 article 的 title 属性
+        $article->title = $request->get('title');
+        // 同上
+        $article->body = $request->get('body');
+        // 获取当前 Auth 系统中注册的用户，并将其 id 赋给 article 的 user_id 属性
+        $article->user_id = $request->user()->id;
+
+        // 将数据保存到数据库，通过判断保存结果，控制页面进行不同跳转
+        if ($article->save()) {
+            // 保存成功，跳转到 文章管理 页
+            return redirect('admin/article');
+        } else {
+            // 保存失败，跳回来路页面，保留用户的输入，并给出提示
+            return redirect()->back()->withInput()->withErrors('保存失败！');
+        }
     }
 
     /**
@@ -66,6 +92,7 @@ class ArticleController extends Controller
     public function edit($id)
     {
         //
+        return view('admin.article.edit')->withArticle(Article::find($id));
     }
 
     /**
