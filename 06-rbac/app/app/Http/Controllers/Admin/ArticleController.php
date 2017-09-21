945abe6f72a12f87b3cdd208bfc6cd20f418090c
坚硬的石头 <do_nb@outlook.com>
2017-09-22 04:21:25 +0800

    [06-rbac] 添加 /admin/{id}/edit 视图

--- 06-rbac/app/app/Http/Controllers/Admin/ArticleController.php
+++ 06-rbac/app/app/Http/Controllers/Admin/ArticleController.php
@@ -105,6 +105,20 @@ class ArticleController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $this->validate($request, [
+            'title' => 'required|unique:articles,title,'.$id.'|max:255',
+            'body' => 'required',
+        ]);
+
+        $article = Article::find($id);
+        $article->title = $request->get('title');
+        $article->body = $request->get('body');
+
+        if ($article->save()) {
+            return redirect('admin/article');
+        } else {
+            return redirect()->back()->withInput()->withErrors('更新失败！');
+        }
     }
 
     /**
